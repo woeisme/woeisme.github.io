972f0572f880abe3d27d5863a2f0c26168dec5b6
James Polk <github@damnfbi.tk>
2011-10-18 05:14:57 -0400

    added post,fixed script

--- post_creater.sh
+++ post_creater.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cur_date=`date +%Y-%m-%d`
 echo $cur_date
+while [[ $answer1 != y ]]
+	do
 echo "Create new post? y/n"
 read answer1
 
@@ -8,26 +10,22 @@ if [ $answer1 == y ]
 	then
 	echo "Enter title for post"
 	read title1
-	title2=`echo $title1 |sed -e 's/ /-/g'`
-	post="${cur_date}-${title2}.markdown"
-	touch "_/posts/$post"
-else 
-	exit
-fi
-echo -e "---\r\nlayout: default\r\ntitle: $title1\r\n---">_posts/$post
-vi _/posts/${post}
-git add _posts/$posts
-echo "enter a comment for the post"
-read comment
-git commit -a -m"$comment"
-echo "push to git? y/n"
-read answer2
-if [ $answer2 == y ]
+	echo $title1
+	title2="${title1// /-}"
+	echo $title2
+	post="${cur_date}-${title2}.md"
+	echo $post
+	touch "_posts/${post}"
+
+elif [ $answer1 == n ]
 	then
-	git push 
-	echo "done, exiting"
+	echo "goodbye"
 	exit
-else	
-	echo "ok exiting ,post is stored already"
-	exit
+else 
+	echo "I'm not sure what you entered, try again"
+	sleep 2
+	clear
 fi
+done
+echo "done touching"
+echo -e "---\r\nlayout: default\r\ntitle: $title1\r\n---">_posts/${post}
